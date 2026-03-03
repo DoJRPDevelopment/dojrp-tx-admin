950c6b946f06327a58bb708376dee6362665726d
Jacob P. <jacobwpaulin@gmail.com>
2026-03-03 18:00:48 -0500

    fix: fixed dojrp middleware

--- core/modules/WebServer/middlewares/authMws.ts
+++ core/modules/WebServer/middlewares/authMws.ts
@@ -190,22 +190,14 @@ export const apiAuthMw = async (ctx: InitializedCtx, next: Function) => {
 export const apiAuthMwDojrp = async (ctx: InitializedCtx, next: Function) => {
     const sendTypedResp = (data: ApiAuthErrorResp | (ApiToastResp & GenericApiErrorResp)) => ctx.send(data);
 
-    //Check auth
-    const authResult = checkRequestAuth(
-        ctx.request.headers,
-        ctx.ip,
-        ctx.txVars.isLocalRequest,
-        ctx.sessTools
-    );
-    if (!authResult.success) {
-        ctx.sessTools.destroy();
-        if (authResult.rejectReason && (authResult.rejectReason !== 'nui_admin_not_found' || console.isVerbose)) {
-            console.verbose.warn(`Invalid session auth: ${authResult.rejectReason}`);
+    if ("string" === typeof ctx.request.headers["x-dojrp-txadmin-request"]) {
+        if ("51.81.71.212" !== ctx.ip || "true" !== ctx.request.headers["x-dojrp-txadmin-request"]) {
+            ctx.sessTools.destroy();
+            return sendTypedResp({
+                logout: true,
+                reason: "Rejected"
+            });
         }
-        return sendTypedResp({
-            logout: true,
-            reason: authResult.rejectReason ?? 'no session'
-        });
     }
 
     await next();
